bfbbf766f164c58247db91432fe3042eff0c0916
malarsaravana7 <75508395+malarsaravana7@users.noreply.github.com>
2021-07-15 18:19:17 -0700

    ll

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -53,7 +53,7 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
-       hooks.event("written")
+        hooks.event("written")
 
 class ReStructuredTextParser(Parser):
     file_exts = [".rst"]
